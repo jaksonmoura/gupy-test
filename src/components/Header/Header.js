ffd77de99de08a920918a68852c709361d968c97
Jakson Moura <jrochelly@gmail.com>
2022-03-18 00:21:36 -0300

    Add mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../../logo.svg";
+import menuicon from "../../assets/img/menu.svg";
 import { Button } from "../Misc.style";
 import * as S from "./Header.style";
 
 const Header = () => {
+	const handleEvents = () => {
+		const menu = document.querySelector("#menu");
+		const overlay = document.querySelector("#menu-overlay");
+		const nav = document.querySelector("header nav");
+		menu.addEventListener("click", (e) => {
+			e.preventDefault();
+			overlay.classList.add("active");
+			nav.classList.add("open");
+		});
+		overlay.addEventListener("click", (e) => {
+			e.preventDefault();
+			overlay.classList.remove("active");
+			nav.classList.remove("open");
+		});
+	};
+
+	useEffect(() => {
+		handleEvents();
+	});
+
 	return (
 		<S.Header>
 			<div className="container">
 				<a href="#" className="logo">
 					<img src={logo} alt="Gupy" />
+				</a>
+				<a href="#menu" id="menu">
+					<img src={menuicon} alt="MENU" />
 				</a>
 				<nav>
 					<ul>
@@ -29,6 +53,7 @@ const Header = () => {
 					</ul>
 				</nav>
 			</div>
+			<div id="menu-overlay"></div>
 		</S.Header>
 	);
 };
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -8,6 +8,63 @@ export const Header = styled.header`
 	justify-content: center;
 	align-items: center;
 
+	#menu,
+	#menu-overlay {
+		opacity: 0;
+		visibility: hidden;
+		pointer-events: none;
+		transition: 0.3s ease-in-out;
+	}
+
+	#menu-overlay.active {
+		opacity: 1;
+		visibility: visible;
+		pointer-events: all;
+	}
+
+	@media screen and (max-width: 992px) {
+		background: #fff6e5;
+		border-bottom: 0;
+
+		#menu {
+			opacity: 1;
+			visibility: visible;
+			pointer-events: all;
+		}
+
+		nav {
+			position: absolute;
+			width: calc(100vw - 80px);
+			max-width: 400px;
+			height: 100vh;
+			background: #fff;
+			top: 0;
+			right: 0;
+			z-index: 999;
+			padding: 1rem;
+			transform: translateX(110%);
+
+			&.open {
+				transform: translateX(0);
+			}
+
+			ul {
+				flex-direction: column;
+				gap: 1rem !important;
+			}
+		}
+
+		#menu-overlay {
+			position: absolute;
+			width: 100vw;
+			height: 100vh;
+			background: rgba(0, 0, 0, 0.3);
+			top: 0;
+			right: 0;
+			z-index: 998;
+		}
+	}
+
 	.container {
 		display: inline-flex;
 		justify-content: space-between;
@@ -20,16 +77,20 @@ export const Header = styled.header`
 		color: #888888;
 	}
 
-	nav ul {
-		display: inline-flex;
-		align-items: center;
-		gap: 32px;
-		font-size: 13px;
+	nav {
+		transition: 0.3s ease-in-out;
 
-		li {
-			height: 52px;
-			&:not(:last-of-type) {
-				line-height: 52px;
+		ul {
+			display: inline-flex;
+			align-items: center;
+			gap: 32px;
+			font-size: 13px;
+
+			li {
+				height: 52px;
+				&:not(:last-of-type) {
+					line-height: 52px;
+				}
 			}
 		}
 	}
