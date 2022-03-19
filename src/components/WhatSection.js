0c07c89b4a0b0dadbcb7e98d288c39d5bf9cd065
Jakson Moura <jrochelly@gmail.com>
2022-03-19 13:23:29 -0300

    Finised component

--- src/components/WhatSection.js
+++ src/components/WhatSection.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import starimg from "../assets/img/rh/rh_star_color.png";
 import * as S from "./WhatSection.style";
+import rh_barra from "../assets/img/rh/rh_bar.svg";
+import rh_desenvolver from "../assets/img/rh/rh_1.png";
+import rh_encantar from "../assets/img/rh/rh_2.png";
+import rh_inovar from "../assets/img/rh/rh_3.png";
+import rh_atrair from "../assets/img/rh/rh_4.png";
+import rh_carrousel_star from "../assets/img/rh/rh_carrousel_star.svg";
+import rh_arrow from "../assets/img/arrow.svg";
 
 const WhatSection = () => {
+	const [carrouselCurrIndex, setCarrouselCurrIndex] = useState(0);
+	const [carrouselSlides, setCarrouselSlides] = useState([
+		{ title: "Desenvolver", deg: 0, color: "#FF8D5C" },
+		{ title: "Encantar", deg: 90, color: "#FFCF72" },
+		{ title: "Inovar", deg: 180, color: "#27ADD9" },
+		{ title: "Atrair", deg: 270, color: "#FF607C" },
+	]);
+
+	const carrouselCtrlClick = (e, reverse = false) => {
+		e.preventDefault();
+		let nextIndex;
+		if (reverse) {
+			nextIndex =
+				carrouselCurrIndex === 0
+					? carrouselSlides.length - 1
+					: carrouselCurrIndex - 1;
+		} else {
+			nextIndex =
+				carrouselCurrIndex + 1 >= carrouselSlides.length
+					? 0
+					: carrouselCurrIndex + 1;
+		}
+		setCarrouselCurrIndex(nextIndex);
+	};
+
+	const changeCurrIndex = (e, index) => {
+		e.preventDefault();
+		setCarrouselCurrIndex(index);
+	};
+
 	return (
 		<S.Section>
 			<div className="star">
@@ -24,8 +61,85 @@ const WhatSection = () => {
 					</defs>
 				</svg>
 			</div>
+
 			<div className="container">
-				<h2>O que é</h2>
+				<div>
+					<h2>O que é um RH que inspira?</h2>
+					<p>
+						Um RH que inspira é aquele que entende que o colaborador
+						é a alma do negócio e investe seus recursos e esforços
+						para que ele tenha uma experiência incrível na sua
+						jornada profissional.
+					</p>
+				</div>
+				<S.Carrousel>
+					<div
+						className="carrousel-star"
+						style={{
+							backgroundColor:
+								carrouselSlides[carrouselCurrIndex].color,
+						}}
+					></div>
+					<S.CarrouselControls>
+						<S.CarrouselArrow
+							onClick={(e) => carrouselCtrlClick(e, true)}
+							href="#"
+						>
+							<img src={rh_arrow} alt="Slide anterior" />
+						</S.CarrouselArrow>
+						<S.CarrouselTitle>
+							{carrouselSlides[carrouselCurrIndex].title}
+						</S.CarrouselTitle>
+						<S.CarrouselArrow
+							onClick={(e) => carrouselCtrlClick(e)}
+							href="#"
+						>
+							<img src={rh_arrow} alt="Próximo slide" />
+						</S.CarrouselArrow>
+					</S.CarrouselControls>
+
+					<S.CarrouselSlides
+						style={{
+							transform: `rotate(-${carrouselSlides[carrouselCurrIndex].deg}deg)`,
+						}}
+					>
+						<a
+							href="#"
+							className="slide"
+							onClick={(e) => changeCurrIndex(e, 0)}
+						>
+							<img
+								src={rh_desenvolver}
+								alt=""
+								id="slide-desenvolver"
+							/>
+						</a>
+						<a
+							href="#"
+							className="slide"
+							onClick={(e) => changeCurrIndex(e, 1)}
+						>
+							<img src={rh_encantar} alt="" id="slide-encantar" />
+						</a>
+						<a
+							href="#"
+							className="slide"
+							onClick={(e) => changeCurrIndex(e, 2)}
+						>
+							<img src={rh_inovar} alt="" id="slide-inovar" />
+						</a>
+						<a
+							href="#"
+							className="slide"
+							onClick={(e) => changeCurrIndex(e, 3)}
+						>
+							<img src={rh_atrair} alt="" id="slide-atrair" />
+						</a>
+					</S.CarrouselSlides>
+				</S.Carrousel>
+			</div>
+			<div className="section-bar">
+				<img src={rh_barra} alt="" />
 			</div>
 		</S.Section>
 	);
--- src/components/WhatSection.style.js
+++ src/components/WhatSection.style.js
@@ -3,17 +3,227 @@ import { SectionDefault } from "./Misc.style";
 
 export const Section = styled(SectionDefault)`
 	background: #fffaf0;
+	position: relative;
+	padding: 95px;
+	height: 475px;
 
 	.star {
-		animation: rotate 60s linear infinite reverse;
+		animation: rotate 30s linear infinite reverse;
 		transform-origin: 50% 50%;
+		will-change: transform;
+		width: 250px;
+		height: 250px;
+		position: absolute;
+		top: -125px;
+		left: -70px;
+		z-index: 0;
 		#star_color {
 			clip-path: url(#star);
 		}
 
 		svg path {
-			animation: rotate 33s linear infinite;
+			will-change: transform;
+			animation: rotate 5s linear infinite;
 			transform-origin: 50% 50%;
 		}
 	}
+
+	.container {
+		display: grid;
+		grid-template-columns: 360px auto;
+		gap: 120px;
+		z-index: 1;
+	}
+
+	.section-bar {
+		position: absolute;
+		bottom: -26px;
+		height: 26px;
+		width: 100%;
+		left: 0;
+		z-index: -1;
+		img {
+			width: 100%;
+		}
+	}
+
+	.carrousel-star {
+		position: absolute;
+		bottom: 125px;
+		left: 210px;
+		width: 105px;
+		height: 105px;
+		transition: all 0.5s ease-in-out;
+		z-index: 1;
+
+		mask: url(rh_carrousel_star.svg) no-repeat center / contain;
+	}
+
+	@media screen and (max-width: 1100px) {
+		height: auto;
+		padding: 95px 32px;
+		.container {
+			grid-template-columns: 1fr;
+			grid-template-rows: auto auto;
+			gap: 32px;
+		}
+
+		.carrousel-star {
+			left: 55%;
+			bottom: 48px;
+		}
+	}
+
+	@media screen and (max-width: 600px) {
+		.carrousel-star {
+			width: 52px;
+			height: 52px;
+		}
+	}
+`;
+
+export const Carrousel = styled.div`
+	display: inline-flex;
+	align-items: center;
+	gap: 32px;
+	margin-top: -150px;
+	position: relative;
+	transition: 0.3s ease-in-out;
+	@media screen and (max-width: 1100px) {
+		margin-top: 0;
+		flex-direction: column-reverse;
+	}
+`;
+
+export const CarrouselControls = styled.div`
+	display: inline-flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 32px;
+	width: 176px;
+
+	@media screen and (max-width: 1100px) {
+		width: auto;
+		flex-direction: row;
+	}
+`;
+
+export const CarrouselArrow = styled.a`
+	&:last-of-type {
+		transform: rotate(180deg);
+	}
+
+	@media screen and (max-width: 1100px) {
+		&:first-of-type {
+			transform: rotate(-90deg);
+		}
+
+		&:last-of-type {
+			transform: rotate(90deg);
+		}
+	}
+`;
+
+export const CarrouselSlides = styled.div`
+	display: grid;
+	transition: 0.5s ease-in-out;
+	grid-template-areas:
+		"rh2 ."
+		"rh1 rh3"
+		"rh4 .";
+
+	a.slide {
+		align-self: center;
+		justify-self: center;
+		&:nth-of-type(1) {
+			grid-area: rh1;
+			/* margin-right: -100px; */
+			margin-top: -11px;
+		}
+		&:nth-of-type(2) {
+			grid-area: rh2;
+			margin-bottom: -102px;
+			/* margin-right: -11px; */
+			margin-right: calc(-100% - 10px);
+		}
+		&:nth-of-type(3) {
+			grid-area: rh3;
+			/* margin-left: -100px; */
+			margin-bottom: -11px;
+		}
+		&:nth-of-type(4) {
+			grid-area: rh4;
+			margin-top: -102px;
+			/* margin-left: -11px; */
+			margin-left: calc(100% - 10px);
+		}
+	}
+
+	@media screen and (max-width: 1100px) {
+		a.slide {
+			&:nth-of-type(1) img,
+			&:nth-of-type(3) img {
+				width: 45vw;
+				max-width: 300px;
+			}
+			&:nth-of-type(2) img,
+			&:nth-of-type(4) img {
+				height: 45vw;
+				max-height: 300px;
+			}
+			&:nth-of-type(1) {
+				margin-right: 0;
+				margin-top: -10px;
+			}
+			&:nth-of-type(2) {
+				margin-bottom: calc(-40% + 10px);
+				margin-right: calc(-100% - 11px);
+			}
+			&:nth-of-type(3) {
+				margin-left: 0;
+				margin-bottom: -11px;
+			}
+			&:nth-of-type(4) {
+				margin-top: calc(-40% + 10px);
+				margin-left: calc(100% - 11px);
+			}
+		}
+	}
+
+	@media screen and (max-width: 600px) {
+		a.slide {
+			&:nth-of-type(1) img,
+			&:nth-of-type(3) img {
+				width: calc(50vw - 74px);
+			}
+			&:nth-of-type(2) img,
+			&:nth-of-type(4) img {
+				height: calc(50vw - 74px);
+			}
+			&:nth-of-type(1) {
+				margin-right: 0;
+				margin-top: 0;
+			}
+			&:nth-of-type(2) {
+				margin-bottom: -40%;
+				margin-right: calc(-100% - 5px);
+			}
+			&:nth-of-type(3) {
+				margin-left: 0;
+				margin-bottom: -11px;
+			}
+			&:nth-of-type(4) {
+				margin-top: calc(-40% + 5px);
+				margin-left: calc(100% - 5px);
+			}
+		}
+	}
+`;
+
+export const CarrouselTitle = styled.span`
+	font-weight: 700;
+	font-family: "Libre Baskerville", serif;
+	font-size: 1.6rem;
+	width: 125px;
+	text-align: center;
 `;
